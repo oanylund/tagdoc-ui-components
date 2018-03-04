c3dbccc5ec29152b662e679824d7b36828cf7f60
oanylund <ole.a.nylund@gmail.com>
2018-03-04 13:16:12 +0100

    Created compose2, ifElse, when helper components

--- themes/helpers.js
+++ themes/helpers.js
@@ -1,4 +1,5 @@
 import themes from "styled-theming";
+import { css } from "styled-components";
 import PropTypes from "prop-types";
 import Color from "color";
 
@@ -19,15 +20,20 @@ export const mainVariants = (kind, variants) => {
   };
 };
 
-export const compose2 = (a, b) => c => a(b(c));
-
-export const lighten = val => color => 
+export const lighten = val => color =>
   Color(color)
     .lighten(val)
-    .hex()
+    .hex();
 
 export const darken = val => color =>
   Color(color)
     .darken(val)
-    .hex()
+    .hex();
 
+export const compose2 = (a, b) => c => a(b(c));
+
+export const ifElse = predicate => (tru, fals) => props =>
+  predicate(props) ? tru(props) : fals(props);
+
+export const when = predicate => (...styles) => props =>
+  predicate(props) ? css(...styles) : null;
