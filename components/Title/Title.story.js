6fb13c76cf0379d973cec7e90ad6f725b80e2188
oanylund <ole.a.nylund@gmail.com>
2018-03-08 21:20:19 +0100

    Title component MVP

---
+++ components/Title/Title.story.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import centerDecorator from "../../.storybook/decorators/centerDecorator";
+
+import Title from "./Title";
+
+storiesOf("Title", module)
+  .addDecorator(centerDecorator)
+  .add("Sizes", () => (
+    <div>
+      <Title size="small">Small Title</Title>
+      <Title size="normal">Normal Title</Title>
+      <Title size="large">Large Title</Title>
+    </div>
+  ));
