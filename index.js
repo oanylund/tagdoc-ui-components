fe2eba14f4f481c63fdac7d4b0bf28ed3f7d9840
oanylund <ole.a.nylund@gmail.com>
2018-03-18 16:46:57 +0100

    fix(entry): Added entry index file to export components to consumer

---
+++ index.js
@@ -0,0 +1,30 @@
+// Components
+import Button from "./components/Button/Button";
+import Card from "./components/Card/Card";
+import Checkbox from "./components/Checkbox/Checkbox";
+import Modal from "./components/Modal/Modal";
+import Navbar from "./components/Navbar/Navbar";
+import NavItem from "./components/NavItem/NavItem";
+import Portal from "./components/Portal/Portal";
+import Radio from "./components/Radio/Radio";
+import RadioGroup from "./components/RadioGroup/RadioGroup";
+import SubNavbar from "./components/SubNavbar/SubNavbar";
+import SubNavItem from "./components/SubNavItem/SubNavItem";
+import TextInput from "./components/TextInput/TextInput";
+import Title from "./components/Title/Title";
+
+export default {
+  Button,
+  Card,
+  Checkbox,
+  Modal,
+  Navbar,
+  NavItem,
+  Portal,
+  Radio,
+  RadioGroup,
+  SubNavbar,
+  SubNavItem,
+  TextInput,
+  Title
+};
