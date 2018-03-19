09d14d679be3a8de214b16e4095a4ea1b8fec18a
oanylund <ole.a.nylund@gmail.com>
2018-03-19 01:36:11 +0100

    build(webpack): added webpack and babel to transpile and bundle release

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    "styled-components": "styled-components"
+  }
+};
